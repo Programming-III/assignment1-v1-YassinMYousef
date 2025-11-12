085fd1c59df41520a30a59d516cf6d89265d47d3
YassinMYousef <yassinsokkary045@gmail.com>
2025-11-12 15:11:37 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -5,7 +5,21 @@
 using namespace std;
 
 //define visitor class here
-
+class Visitor{
+  private:
+    string visitorName;
+    int ticketsBought;
+  public:
+  
+    Visitor(string n, int t){
+      visitorName = n;
+      ticketsBought = t;
+    }
+    
+    void displayInfo(){
+      cout << "name is " << visitorName << "amount of tickets" << ticketsBought << endl;
+    }
+};
 
 
 
