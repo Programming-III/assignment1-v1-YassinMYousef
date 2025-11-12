dac7233bd35fb6b57df3ce4f4dc129b80874e3f2
YassinMYousef <yassinsokkary045@gmail.com>
2025-11-12 15:08:49 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -4,7 +4,13 @@
 #include "Animal.h"
 
 //define class bird here 
-
+class Bird : public Animal {
+  string wingSpan;
+  
+  public Bird(string n , int a , bool h , string w) : Animal (n , a , h){
+    wingSpan = w;
+  }
+};
 
 
 
