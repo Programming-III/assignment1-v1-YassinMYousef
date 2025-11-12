094ff76ba67debf7c5a6e9fff7fbfa2df5f47573
YassinMYousef <yassinsokkary045@gmail.com>
2025-11-12 15:10:00 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -6,6 +6,30 @@
 //define enclosure class here 
 
 
+class Enclosure{
+  private:
+    Animal* Animal;
+    int capacity;
+    int currentCount;
+
+  public:
+  
+    Enclosure(Animal* a, int c , int cc){
+      Animal = a;
+      int capacity = c;
+      int currentCount = cc;
+     
+    }
+    
+    void addAnimal(Animal* a){
+      
+    }
+    
+    
+    void displayAnimals(){
+      
+    }
+};
 
 
 
