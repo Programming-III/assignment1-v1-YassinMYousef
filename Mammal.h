d619b7977aaef0ba579a1fcd253b558e1b0bcb64
YassinMYousef <yassinsokkary045@gmail.com>
2025-11-12 15:10:34 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,14 @@
 #include "Animal.h"
 
 //define mammal class here
-
+class Mammel : public Animal{
+  string furColor;
+  
+  public Mammel(string n , int a , bool h , string f) : Animal (n , a , h){
+    furColor = f;
+  }
+  
+};
 
 
 
