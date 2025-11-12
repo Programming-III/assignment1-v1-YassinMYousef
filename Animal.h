539ea6926ebeda6aedf61be68c5af842c812232d
YassinMYousef <yassinsokkary045@gmail.com>
2025-11-12 15:04:03 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -5,7 +5,34 @@
 using namespace std;
 
 //define animal class here 
-
+class Animal{
+  private:
+    string name;
+    int age;
+    bool isHungry;
+    
+  public: 
+  
+    Animal(string n , int a , bool h){
+      name = n;
+      age = a;
+      isHungry = h;
+    }
+    
+    void display(){
+      cout << "Name is " << name << "age is " << age << " hunger status" <<  isHungry <<endl;
+    }
+    
+    void feed(){
+      if(x.isHungry == true)
+      cout << "isnt hungry" << endl;
+      
+      else 
+        cout << "is hungry"<< endl;
+      
+    }
+  
+};
 
 
 #endif
