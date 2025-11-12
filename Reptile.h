529943b1785bb3a44f45c0768abb1cad6b68d414
YassinMYousef <yassinsokkary045@gmail.com>
2025-11-12 15:11:03 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,7 +5,13 @@
 
 //define reptile class here
 
-
+class Reptile : public Animal{
+  bool isVenomous;
+  
+  public Mammel(string n , int a , bool h , bool v) : Animal (n , a , h){
+    isVenomous = v;
+  }
+};
 
 
 
